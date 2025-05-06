36f3c54ec38b1e01d58045d85b19bd9fb5866e91
Touhid <touhidulislamtawhid@gmail.com>
2025-05-06 15:07:07 +0600

    moved to env

--- src/api/legendHunt.ts
+++ src/api/legendHunt.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../auth/authUtils";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/legend_hunt/",
+  baseURL: import.meta.env.VITE_API_DOMAIN,
 });
 
 api.interceptors.request.use((config) => {
@@ -14,8 +14,8 @@ api.interceptors.request.use((config) => {
 });
 
 export const getBalance = () =>
-  api.get("/usdt-balance/").then((res) => res.data);
+  api.get("/api/legend_hunt/usdt-balance/").then((res) => res.data);
 export const startGame = (bet_amount: number) =>
-  api.post("/start-game/", { bet_amount });
+  api.post("/api/legend_hunt/start-game/", { bet_amount });
 export const flipCard = (gameId: number) =>
-  api.post(`/flip-card/${gameId}/`).then((res) => res.data);
+  api.post(`/api/legend_hunt/flip-card/${gameId}/`).then((res) => res.data);
